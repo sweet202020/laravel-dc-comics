d66aebcd731633db31f323b4a25d8573617f93b7
sweet202020 <dolcealessio1@gmail.com>
2022-12-21 18:23:45 +0100

    fix header title

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <h1 class="text-center text-uppercase mt-3">train station</h1>
+    <h1 class="text-center text-uppercase mt-3">dc comics</h1>
     <nav class="nav justify-content-center text-uppercase ">
         <a class="m-3 {{Route::currentRouteName() === 'home' ? 'active' : ''}}" href="/" aria-currentRoute="page">home</a>
         <a class="m-3 {{Route::currentRouteName() === 'about' ? 'active' : ''}}" href="{{route('about')}}">about</a>
